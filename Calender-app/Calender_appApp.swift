e64626e58136f81fc6fe642ab9cdb10f1582cfbf
kenaono <aoken.contact@gmail.com>
2024-10-10 00:04:34 +0900

    Initial Commit

---
+++ Calender-app/Calender_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calender_appApp.swift
+//  Calender-app
+//
+//  Created by 安藤藍花 on 2024/10/09.
+//
+
+import SwiftUI
+
+@main
+struct Calender_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calender-appTests/Calender_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calender_appTests.swift
+//  Calender-appTests
+//
+//  Created by 安藤藍花 on 2024/10/09.
+//
+
+import Testing
+@testable import Calender_app
+
+struct Calender_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
